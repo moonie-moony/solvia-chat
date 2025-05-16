81b3d877155bd1345a6710706fa7a030fca7249d
moonie-moony <lekajteo40@gmail.com>
2025-05-16 19:34:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,24 @@ const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
+const users = new Set();
+
 io.on("connection", (socket) => {
   console.log("A user connected");
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+
+  socket.on("join", (username) => {
+    users.add(username);
+    console.log(`${username} joined`);
   });
+
+  socket.on("chat message", (data) => {
+    // data = { username, message }
+    io.emit("chat message", data);
+  });
+
   socket.on("disconnect", () => {
     console.log("A user disconnected");
+    // Optional: remove username from users Set here if you track usernames by socket
   });
 });
 
