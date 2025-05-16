c6fc85e6a4d3da97801de31560014e381b9c0f71
moonie-moony <lekajteo40@gmail.com>
2025-05-16 19:44:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,78 +8,68 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-// Map to track online users: username -> socket.id
-const onlineUsers = new Map();
-
-// Map to store each user’s friends: username -> Set of friends
-const friendsMap = new Map();
+const users = {};
+const friends = {};
 
 io.on("connection", (socket) => {
   let currentUser = null;
 
-  // User joins with a username
   socket.on("join", (username) => {
     currentUser = username;
-    onlineUsers.set(username, socket.id);
+    users[currentUser] = socket.id;
 
-    // Initialize friends set if new user
-    if (!friendsMap.has(username)) friendsMap.set(username, new Set());
+    if (!friends[currentUser]) friends[currentUser] = [];
 
-    // Send friend statuses to the user
-    const friends = Array.from(friendsMap.get(username));
-    const friendStatuses = friends.map(friendName => ({
+    // Send current friends status to user
+    const friendsStatus = friends[currentUser].map((friendName) => ({
       name: friendName,
-      online: onlineUsers.has(friendName)
+      online: users[friendName] !== undefined,
     }));
-    socket.emit("friendsStatus", friendStatuses);
 
-    // Notify this user’s friends that they are now online
-    friends.forEach(friendName => {
-      const friendSocketId = onlineUsers.get(friendName);
-      if (friendSocketId) {
-        io.to(friendSocketId).emit("friendStatusUpdate", { name: username, online: true });
-      }
-    });
-
-    // Optionally join a public "lobby" room for group chat
+    socket.emit("friendsStatus", friendsStatus);
     socket.join("lobby");
     socket.emit("joinedRoom", "lobby");
+
+    // Notify friends that user is online
+    friends[currentUser].forEach((friendName) => {
+      const friendSocketId = users[friendName];
+      if (friendSocketId) {
+        io.to(friendSocketId).emit("friendStatusUpdate", {
+          name: currentUser,
+          online: true,
+        });
+      }
+    });
   });
 
-  // Handle friend request / add friend
   socket.on("addFriend", (friendName) => {
-    if (!currentUser) return;
-
-    // Add friend both ways (simple symmetric friendship)
-    friendsMap.get(currentUser).add(friendName);
-    if (!friendsMap.has(friendName)) friendsMap.set(friendName, new Set());
-    friendsMap.get(friendName).add(currentUser);
-
-    // Inform current user that friend was added
-    socket.emit("friendAdded", { name: friendName, online: onlineUsers.has(friendName) });
-
-    // Notify friend (if online) they got a new friend
-    const friendSocketId = onlineUsers.get(friendName);
-    if (friendSocketId) {
-      io.to(friendSocketId).emit("friendAdded", { name: currentUser, online: true });
+    if (!friends[currentUser].includes(friendName)) {
+      friends[currentUser].push(friendName);
+      socket.emit("friendAdded", { name: friendName, online: users[friendName] !== undefined });
     }
   });
 
-  // Handle user disconnecting
+  socket.on("chatMessage", (msg) => {
+    io.to("lobby").emit("chatMessage", { user: currentUser, message: msg });
+  });
+
   socket.on("disconnect", () => {
-    if (!currentUser) return;
+    if (currentUser) {
+      delete users[currentUser];
 
-    // Remove user from online users
-    onlineUsers.delete(currentUser);
-
-    // Notify friends user went offline
-    const friends = Array.from(friendsMap.get(currentUser) || []);
-    friends.forEach(friendName => {
-      const friendSocketId = onlineUsers.get(friendName);
-      if (friendSocketId) {
-        io.to(friendSocketId).emit("friendStatusUpdate", { name: currentUser, online: false });
+      // Notify friends that user is offline
+      if (friends[currentUser]) {
+        friends[currentUser].forEach((friendName) => {
+          const friendSocketId = users[friendName];
+          if (friendSocketId) {
+            io.to(friendSocketId).emit("friendStatusUpdate", {
+              name: currentUser,
+              online: false,
+            });
+          }
+        });
       }
-    });
+    }
   });
 });
 
