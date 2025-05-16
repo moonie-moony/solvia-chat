f962e743294ce0c7d2044e29cc833c472691ed81
moonie-moony <lekajteo40@gmail.com>
2025-05-16 19:35:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,24 +5,84 @@ const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
-const users = new Set();
+// Data structures to track users, friends, groups
+const users = new Map(); // socket.id -> { username, friends: Set, currentRoom }
+const groups = new Map(); // roomName -> Set of usernames
+
+// Default global chat room
+const GLOBAL_ROOM = "lobby";
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  console.log("A user connected", socket.id);
 
+  // User joins with username
   socket.on("join", (username) => {
-    users.add(username);
-    console.log(`${username} joined`);
+    users.set(socket.id, { username, friends: new Set(), currentRoom: GLOBAL_ROOM });
+    socket.join(GLOBAL_ROOM);
+    console.log(`${username} joined and entered ${GLOBAL_ROOM}`);
+
+    // Send current room info to user
+    socket.emit("joinedRoom", GLOBAL_ROOM);
+
+    // Notify others in the room
+    socket.to(GLOBAL_ROOM).emit("chat message", { username: "System", message: `${username} joined the room.` });
   });
 
+  // Handle chat message
   socket.on("chat message", (data) => {
-    // data = { username, message }
-    io.emit("chat message", data);
+    // data = { message }
+    const user = users.get(socket.id);
+    if (!user) return;
+    const room = user.currentRoom;
+    io.to(room).emit("chat message", { username: user.username, message: data.message });
+  });
+
+  // Handle switching rooms/groups
+  socket.on("joinRoom", (roomName) => {
+    const user = users.get(socket.id);
+    if (!user) return;
+
+    const oldRoom = user.currentRoom;
+    socket.leave(oldRoom);
+    socket.join(roomName);
+
+    user.currentRoom = roomName;
+
+    if (!groups.has(roomName)) {
+      groups.set(roomName, new Set());
+    }
+    groups.get(roomName).add(user.username);
+
+    socket.emit("joinedRoom", roomName);
+    socket.to(oldRoom).emit("chat message", { username: "System", message: `${user.username} left the room.` });
+    socket.to(roomName).emit("chat message", { username: "System", message: `${user.username} joined the room.` });
+  });
+
+  // Handle adding friend
+  socket.on("addFriend", (friendName) => {
+    const user = users.get(socket.id);
+    if (!user) return;
+
+    user.friends.add(friendName);
+    socket.emit("friendAdded", friendName);
   });
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected");
-    // Optional: remove username from users Set here if you track usernames by socket
+    const user = users.get(socket.id);
+    if (user) {
+      console.log(`${user.username} disconnected`);
+      const room = user.currentRoom;
+      socket.to(room).emit("chat message", { username: "System", message: `${user.username} left the room.` });
+      users.delete(socket.id);
+
+      // Remove user from any groups they belonged to
+      for (const [roomName, members] of groups.entries()) {
+        members.delete(user.username);
+        if (members.size === 0) {
+          groups.delete(roomName);
+        }
+      }
+    }
   });
 });
 
