92b63ba8c3277f3ede581b2ca2f784103c9b0b0e
moonie-moony <lekajteo40@gmail.com>
2025-05-16 20:02:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,95 +1,66 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require("http").createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http);
+
+const users = new Map();
+const friendships = new Map();
 
 app.use(express.static("public"));
 
-const PORT = process.env.PORT || 3000;
-
-// Store users: socket.id => { username, friends: Set }
-const users = new Map();
-
 io.on("connection", (socket) => {
-  console.log("User connected: " + socket.id);
+  let username = null;
 
-  socket.on("register", (username) => {
-    users.set(socket.id, { username, friends: new Set() });
-    // Broadcast user list update for friend adding
-    broadcastUserList();
+  socket.on("login", (name) => {
+    username = name;
+    users.set(username, socket);
+    if (!friendships.has(username)) friendships.set(username, []);
+    for (let [otherName, otherSocket] of users.entries()) {
+      if (otherName !== username) {
+        otherSocket.emit("friendRequest", username);
+      }
+    }
   });
 
-  socket.on("getUsers", () => {
-    // Send back the list of all connected users
-    const allUsers = Array.from(users.values()).map(u => u.username);
-    socket.emit("userList", allUsers);
+  socket.on("publicMessage", (msg) => {
+    io.emit("publicMessage", { sender: username, message: msg });
   });
 
-  socket.on("addFriend", (friendName) => {
-    const user = users.get(socket.id);
-    if (!user) return;
-    if (user.username === friendName) return; // cannot add yourself
-
-    // Find friend's socket id
-    const friendEntry = Array.from(users.entries()).find(([_, u]) => u.username === friendName);
-    if (!friendEntry) return;
-
-    user.friends.add(friendName);
-
-    // Notify this socket about new friend
-    socket.emit("friendAdded", friendName);
+  socket.on("privateMessage", ({ to, message }) => {
+    const recipient = users.get(to);
+    if (recipient) {
+      recipient.emit("privateMessage", { from: username, message });
+    }
   });
 
-  socket.on("sendPublicMessage", (message) => {
-    const user = users.get(socket.id);
-    if (!user) return;
+  socket.on("acceptFriend", (from) => {
+    const fList = friendships.get(username);
+    if (!fList.includes(from)) {
+      fList.push(from);
+    }
 
-    const msgObj = {
-      from: user.username,
-      message,
-      timestamp: new Date().toISOString(),
-    };
+    const fromList = friendships.get(from) || [];
+    if (!fromList.includes(username)) {
+      fromList.push(username);
+    }
+    friendships.set(from, fromList);
 
-    io.emit("publicMessage", msgObj);
-  });
+    const otherSocket = users.get(from);
+    if (otherSocket) {
+      otherSocket.emit("friendListUpdate", fromList);
+    }
 
-  socket.on("sendPrivateMessage", ({ to, message }) => {
-    const user = users.get(socket.id);
-    if (!user) return;
-
-    // Find friend socket
-    const friendEntry = Array.from(users.entries()).find(([_, u]) => u.username === to);
-    if (!friendEntry) return;
-
-    const [friendSocketId] = friendEntry;
-
-    const msgObj = {
-      from: user.username,
-      to,
-      message,
-      timestamp: new Date().toISOString(),
-    };
-
-    // Send message to friend and sender
-    io.to(friendSocketId).emit("privateMessage", msgObj);
-    socket.emit("privateMessage", msgObj);
+    socket.emit("friendListUpdate", fList);
   });
 
   socket.on("disconnect", () => {
-    users.delete(socket.id);
-    broadcastUserList();
-    console.log("User disconnected: " + socket.id);
+    if (username) {
+      users.delete(username);
+    }
   });
-
-  function broadcastUserList() {
-    const allUsers = Array.from(users.values()).map(u => u.username);
-    io.emit("userList", allUsers);
-  }
 });
 
-server.listen(PORT, () => {
-  console.log(`Solvia chat server running on port ${PORT}`);
+http.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running...");
 });
