6e4fb189495b3ca240084d76777e4908ba5520e0
moonie-moony <lekajteo40@gmail.com>
2025-05-16 19:50:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,25 +10,21 @@ app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 
-// This will store users and their friend rooms (simplified for demo)
 const users = new Map();
 
 io.on("connection", (socket) => {
   console.log("User connected: " + socket.id);
 
-  // Save username & friends from client
   socket.on("register", ({ username, friends }) => {
     users.set(socket.id, { username, friends });
     console.log(`User registered: ${username}, friends: ${friends}`);
   });
 
-  // Join private room with friendID
   socket.on("joinRoom", (room) => {
     socket.join(room);
     console.log(`${socket.id} joined room ${room}`);
   });
 
-  // Handle private messages only to room members
   socket.on("privateMessage", ({ room, message, from }) => {
     io.to(room).emit("privateMessage", { message, from });
   });
