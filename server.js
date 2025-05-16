d7bb68d8e3597a88f49cf09e056fb39b42094bd1
moonie-moony <lekajteo40@gmail.com>
2025-05-16 20:41:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,68 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-const PORT = 3000;
+app.use(express.static("public"));
 
-// Serve static files from current folder
-app.use(express.static(__dirname));
+const users = new Set();
 
-// Data storage (in-memory)
-let users = {};
-let friendRequests = {};
-let friends = {};
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
-io.on('connection', socket => {
-  console.log('User connected:', socket.id);
-
-  // When user joins with username
-  socket.on('join', username => {
-    socket.username = username;
-    users[username] = socket.id;
-    if (!friends[username]) friends[username] = [];
-    if (!friendRequests[username]) friendRequests[username] = [];
-
-    // Send current friends and requests to client
-    socket.emit('friendsList', friends[username]);
-    socket.emit('friendRequests', friendRequests[username]);
+  socket.on("join", (username) => {
+    users.add(username);
+    console.log(`${username} joined`);
   });
 
-  // Handle friend request
-  socket.on('friendRequest', ({ from, to }) => {
-    if (!friendRequests[to]) friendRequests[to] = [];
-    if (!friendRequests[to].includes(from)) {
-      friendRequests[to].push(from);
-      const toSocketId = users[to];
-      if (toSocketId) {
-        io.to(toSocketId).emit('friendRequests', friendRequests[to]);
-      }
-    }
+  socket.on("chat message", (data) => {
+    // data = { username, message }
+    io.emit("chat message", data);
   });
 
-  // Handle public chat message
-  socket.on('publicMessage', msg => {
-    io.emit('publicMessage', msg);
-  });
-
-  // Handle private chat message
-  socket.on('privateMessage', msg => {
-    const toSocketId = users[msg.to];
-    if (toSocketId) {
-      io.to(toSocketId).emit('privateMessage', msg);
-    }
-    // Also send to sender to show in their chat window
-    socket.emit('privateMessage', msg);
-  });
-
-  socket.on('disconnect', () => {
-    if (socket.username) {
-      console.log('User disconnected:', socket.username);
-      delete users[socket.username];
-    }
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+    // Optional: remove username from users Set here if you track usernames by socket
   });
 });
 
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
